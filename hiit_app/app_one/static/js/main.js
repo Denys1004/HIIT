fcb1906f59eb44dd4aafb1b1b600c48c605f954a
Drew Adorno <drew.adorno@gmail.com>
2020-08-05 23:23:25 -0400

    added jquery and sending context directly to js through ajax

--- hiit_app/app_one/static/js/main.js
+++ hiit_app/app_one/static/js/main.js
@@ -3,40 +3,23 @@ var workoutTime
 var breakTime
 var training_exercises
 
-window.onload = () =>{
-    
-    getSession();
-    training();
+window.onload = () =>{  
+    training()
 }
-
-// making usable array from users input:
-// function delete_quotes(ex_string){
-//   let newstr = ex_string.replace(/&#39;/g, "")
-//   newstr = newstr.replace("[", "")
-//   newstr = newstr.replace("]", "")
-//   newstr = newstr.split(", ")
-//   return newstr
-// }
-// training_exercises = delete_quotes(training_exercises)
-// Adding finish image to the training
-// training_exercises.push('finish')
-console.log(training_exercises);
-
-// 3. Countdown
-function countDown(count_time, status, exersise){
+function countDown(count_time, status, exercise){
   return new Promise((resolve) =>{
     let timerNumber = setInterval(() => {
       count_time--;
-      document.getElementById("timerNumber").innerHTML = count_time;
-      document.getElementById("workout_status").innerHTML = status;
+      $("#timerNumber").html(count_time);
+      $("#workout_status").html(status);
       if (status == "WORKOUT"){
-          document.getElementById("timerNumber").style.color = "red";
-          document.getElementById("workout_status").style.color = "red";
+          $("#timerNumber").css('color','red')
+          $("#workout_status").css('color','red')
       }else if(status == "REST" || status == "PREPARE" || status == "BREAK"){
-        document.getElementById("timerNumber").style.color = "green";
-        document.getElementById("workout_status").style.color = "green";
+        $("#timerNumber").css('color','green')
+        $("#workout_status").css('color','green')
       }
-      document.getElementById('exercise_photo_container').innerHTML = `<img id='exersise' src='static/images/${exersise}.gif'>`
+      $('#exercise_photo_container').html(`<img id='exercise' src='static/images/${exercise}.gif'>`)
 
       if (count_time <= 0){
         clearInterval(timerNumber);
@@ -45,35 +28,36 @@ function countDown(count_time, status, exersise){
     }, 1000);
   })
 } 
-
-
-async function training(){
-  await countDown(restTime, 'PREPARE', training_exercises[0])
-  for(let i=0; i<18; i++){
-      if(i % 6==0 && i !=0){
-          await countDown(breakTime, "BREAK", training_exercises[0])
-      }
-      await countDown(workoutTime, "WORKOUT", training_exercises[i%6])
-      if(i != 5 && i !=11 && i !=17){
-          await countDown(restTime, 'REST', training_exercises[i%6+1])
-      }
-  }
-  await countDown(restTime, "DONE", 'finish')
+async function training()
+{
+    await getSession();
+    await countDown(restTime, 'PREPARE', training_exercises[0])
+    for(let i=0; i<18; i++){
+        if(i % 6==0 && i !=0){
+            await countDown(breakTime, "BREAK", training_exercises[0])
+        }
+        await countDown(workoutTime, "WORKOUT", training_exercises[i%6])
+        if(i != 5 && i !=11 && i !=17){
+            await countDown(restTime, 'REST', training_exercises[i%6+1])
+        }
+    }
+    await countDown(restTime, "DONE", 'finish')
 }
-
 async function getSession()
 {
-    $.ajax({
-        type: 'GET',
-        url: '/passSession',
-        success: function(response)
-        {
-            console.log(response);
-            restTime=response.restTime
-            workoutTime=response.workoutTime
-            breakTime=response.breakTime
-            training_exercises=response.training_exercises
-        }
+    return new Promise((resolve) => {
+        $.ajax({
+            type: 'GET',
+            url: '/passSession',
+            success: (response) =>
+            {
+                restTime=response.restTime
+                workoutTime=response.workoutTime
+                breakTime=response.breakTime
+                training_exercises=response.training_exercises
+                resolve()
+            } 
+        })
     })
 }
 
